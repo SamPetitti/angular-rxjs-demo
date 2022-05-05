f32642de1d24d9f4127044765cf269428b4c05d6
Sampjam <sampjam@gmail.com>
2022-05-05 16:55:34 -0400

    rxjs terms and syntx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {of, from} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    of(2,4,6,8).subscribe(item => console.log(item));
+
+    from([1,2,3,4,5,6]).subscribe({
+      next: (item) => console.log('resulting item is...' + item),
+      error: (err) => console.error(`error occurred ${err}`),
+      complete: () => console.log('completed')
+    });
+
+    of('apple', 'banana', 'orange').subscribe({
+      next: (item) => console.log(`resulting item is ${item}`),
+      error: (err) => console.error(`error occured ${err}`),
+      complete: () => console.log('completed')
+    })
+  }
   title = 'angular-rxjs-demo';
 }
